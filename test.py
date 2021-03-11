75b27423fb2bb308730b50403a855e241d7e79e4
yedidyar <yedidyarashi@gmail.com>
2021-03-11 09:53:53 +0200

    all the necessary method complte + test

--- test.py
+++ test.py
@@ -1,15 +1,18 @@
 import requests
 
-BASE =" http://127.0.0.1:5000/"
-data = [{"likes":10,"name":'jeo',"views":5003202},
-        {"likes":100,"name":'hello',"views":5003402},
-        {"likes":10,"name":'me',"views":500042}]
+BASE = " http://127.0.0.1:5000/"
+data = [{"likes": 10, "name": 'jeo', "views": 5003202},
+        {"likes": 100, "name": 'hello', "views": 5003402},
+        {"likes": 10, "name": 'me', "views": 500042}]
 for i in range(len(data)):
     response = requests.put(BASE + "video/" + str(i), data[i])
     print(response.json())
 
-# response = requests.delete(BASE + "video/2")
-# print(response)
-input()
-response = requests.get(BASE + "video/1")
+response = requests.patch(
+    BASE + "video/1")
 print(response.json())
+
+input()
+response = requests.delete(
+    BASE + "video/1")
+print(response)
